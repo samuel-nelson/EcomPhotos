13b3dde1b8928a681a69217cd4cfda0a8eb1f745
Samuel Nelson <44469661+samuel-nelson@users.noreply.github.com>
2026-01-14 21:03:49 -0800

    Fix: make resize width/height optional

--- types/index.ts
+++ types/index.ts
@@ -30,8 +30,8 @@ export interface EditState {
     tint: number;
   };
   resize?: {
-    width: number;
-    height: number;
+    width?: number;
+    height?: number;
     maintainAspectRatio: boolean;
   };
 }
